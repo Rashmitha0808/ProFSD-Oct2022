8e8f0ef8bbae32e5551a37152c1fe93c38a71e95
Rashmitha <100375852+Rashmitha0808@users.noreply.github.com>
2023-04-07 01:19:10 +0530

    updates

--- src/InClassAssignments/Arrays/Donation.java
+++ src/InClassAssignments/Arrays/Donation.java
@@ -1,9 +1,10 @@
 package InClassAssignments.Arrays;
 
 import java.util.Scanner;
-
+// Donation
 public class Donation {
     public static void main(String[] args) {
+        
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
 
